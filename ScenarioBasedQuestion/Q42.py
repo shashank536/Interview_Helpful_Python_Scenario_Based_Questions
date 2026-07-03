f7dd7172954e60137803924d456a9c1a9a20a13a
shashank536 <shashankkumar852@yahoo.com>
2026-07-03 23:02:33 +0530

    Format with black

--- ScenarioBasedQuestion/Q42.py
+++ ScenarioBasedQuestion/Q42.py
@@ -22,17 +22,21 @@ def get_budgets(arr):
     return total
 
 
-print(get_budgets(
-    [
-        {"name": "John", "age": 21, "budget": 23000},
-        {"name": "Steve", "age": 32, "budget": 40000},
-        {"name": "Martin", "age": 16, "budget": 2700},
-    ]
-))
-print(get_budgets(
-    [
-        {"name": "John", "age": 21, "budget": 29000},
-        {"name": "Steve", "age": 32, "budget": 32000},
-        {"name": "Martin", "age": 16, "budget": 1600},
-    ]
-))
+print(
+    get_budgets(
+        [
+            {"name": "John", "age": 21, "budget": 23000},
+            {"name": "Steve", "age": 32, "budget": 40000},
+            {"name": "Martin", "age": 16, "budget": 2700},
+        ]
+    )
+)
+print(
+    get_budgets(
+        [
+            {"name": "John", "age": 21, "budget": 29000},
+            {"name": "Steve", "age": 32, "budget": 32000},
+            {"name": "Martin", "age": 16, "budget": 1600},
+        ]
+    )
+)
